25e91e787b29b2d1fbe5efd049c2d2e7b511a0d6
Moaaz Adel <moaaz.adel@zebra.com>
2024-03-09 13:16:18 +0200

    Add Search Scenarios

---
+++ tests/e2e/search.spec.ts
@@ -0,0 +1,10 @@
+import { test, expect } from "@playwright/test";
+import { HomePage } from "../../pages/HomePage";
+
+test.describe("Search Tests", () => {
+  test("should find matched test results", async ({ page }) => {
+    let homePage: HomePage = new HomePage(page);
+    await homePage.visit();
+    await homePage.searchFor("Bank");
+  });
+});
